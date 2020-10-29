461c3b34c363bbcd8b9b30f70be636b8f764d276
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-29 15:52:01 +0530

    [mrunal] Add. Added test method for calculating cab fare

--- InvoiceCabGeneratorTest/UnitTest1.cs
+++ InvoiceCabGeneratorTest/UnitTest1.cs
@@ -7,5 +7,18 @@ namespace InvoiceCabGeneratorTest
     {
         InvoiceGenerator invoiceGenerator = null;
 
+        [Test]
+        public void GivenDistanceAndTimeReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+
+            double actual = invoiceGenerator.CalculateRideFare(distance, time);
+            double expected = 25;
+
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
